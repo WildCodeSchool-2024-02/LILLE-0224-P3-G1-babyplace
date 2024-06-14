eb369d5ca674c90a26365c4f3d450982cc2c0c39
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-14 11:08:12 +0200

    add links to nursery details and change margin in homepage and nursery details

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         element: <NurseriesSearch />,
       },
       {
-        path: "/NurseryDetails",
+        path: "/creche/details",
         element: <NurseryDetails />,
       },
     ],
