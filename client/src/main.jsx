8b748753c1009e320133d36198302d3d1b7cd65f
PMorganGit <morganpouilly@gmail.com>
2024-06-14 11:10:50 +0200

    link to dashboard

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
         element: <NurseryDetails />,
       },
       {
-        path: "/page2",
+        path: "/dashboard",
         element: <PageDashboard />,
       },
       
