fbd6cc1c74394bec1cd5663ff620822ef6bdff75
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-14 11:42:13 +0200

    change of colors following where you are on the navigation menu in mobile

--- client/src/components/Bottom_navbar/BottomNavbar.jsx
+++ client/src/components/Bottom_navbar/BottomNavbar.jsx
@@ -1,19 +1,54 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./BottomNavbar.css";
 
 function BottomNavbar() {
+  const [homeButton, setHomeButton] = useState(true);
+  const [searchButton, setSearchButton] = useState(false);
+  const [dashboardeButton, setDashboardButton] = useState(false);
+
+  const handleHomeButton = () => {
+    setHomeButton(true);
+    setSearchButton(false);
+    setDashboardButton(false);
+  };
+
+  const handleSearchButton = () => {
+    setHomeButton(false);
+    setSearchButton(true);
+    setDashboardButton(false);
+  };
+
+  const handleDashboardButton = () => {
+    setHomeButton(false);
+    setSearchButton(false);
+    setDashboardButton(true);
+  };
+
   return (
     <div className="all_bottom_navbar">
       <div className="bottom_navbar">
         <div className="all_image_bottom">
-          <Link to="/">
-            <img alt="home" src="assets/images/home_bottom.svg" />
+          <Link to="/" onClick={handleHomeButton}>
+            {homeButton ? (
+              <img alt="home" src="assets/images/home_bottom_active.svg" />
+            ) : (
+              <img alt="home" src="assets/images/home_bottom.svg" />
+            )}
           </Link>
-          <Link to="/creche">
-            <img alt="loop" src="assets/images/loop_bottom.svg" />
+          <Link to="/creche" onClick={handleSearchButton}>
+            {searchButton ? (
+              <img alt="loop" src="assets/images/loop_bottom_active.svg" />
+            ) : (
+              <img alt="loop" src="assets/images/loop_bottom.svg" />
+            )}
           </Link>
-          <Link to="/dashboard">
-            <img alt="avatar" src="assets/images/avatar_bottom.svg" />
+          <Link to="/dashboard" onClick={handleDashboardButton}>
+            {dashboardeButton ? (
+              <img alt="avatar" src="assets/images/avatar_bottom_active.svg" />
+            ) : (
+              <img alt="avatar" src="assets/images/avatar_bottom.svg" />
+            )}
           </Link>
         </div>
       </div>
