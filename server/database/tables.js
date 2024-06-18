56ecfa28ad1045bc49896c0d4a92099a310c9e9f
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-06-18 16:23:33 +0200

    images reorganized

--- server/database/tables.js
+++ server/database/tables.js
@@ -1,6 +1,6 @@
 // Import the repository modules responsible for handling data operations on the tables
 const ParentRepository = require("./models/ParentRepository");
-
+const NurseryRepository = require("./models/NurseryRepository");
 // Create an empty object to hold data repositories for different tables
 const tables = {};
 
@@ -11,6 +11,7 @@ const tables = {};
 // Register each repository as data access point for its table
 tables.parent = new ParentRepository();
 
+tables.nursery = new NurseryRepository();
 /* ************************************************************************* */
 
 // Use a Proxy to customize error messages when trying to access a non-existing table
