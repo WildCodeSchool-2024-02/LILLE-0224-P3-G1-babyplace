6633a5ed332502ddfc5e022bb2a7209d7b6cbe29
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-20 17:24:53 +0200

    continue contact page

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,3 +1,26 @@
+import "./contact.css";
+
 export default function Contact() {
-  return <h2>Contact</h2>;
+  return (
+    <section className="contact_content">
+      <h2>Contact</h2>
+      <p>Vous rencontrez un problème sur notre plateforme ?</p>
+      <p>
+        Vous souhaitez obtenir d'autres informations ? Vous souhaitez signaler
+        un compte ?{" "}
+      </p>
+      <br />
+      <p>
+        Merci de contacter notre équipe, nous vous répondrons dans les plus
+        brefs délais.
+      </p>
+      <form>
+        <label htmlFor="motif">Motif</label>
+        <input type="text" />
+        <label htmlFor="details">Demande</label>
+        <input type="text" />
+        <button type="submit">Envoyer</button>
+      </form>
+    </section>
+  );
 }
