f243ead05dfa3453a56c7517f445e29e71f9ff9d
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-20 16:22:47 +0200

    fix importations svg

--- client/src/components/Bottom_navbar/BottomNavbar.jsx
+++ client/src/components/Bottom_navbar/BottomNavbar.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./BottomNavbar.css";
+import homeSVGActive from "../../../public/assets/images/home_bottom_active.svg";
+import homeSVG from "../../../public/assets/images/home_bottom.svg";
+import loopSVGActive from "../../../public/assets/images/loop_bottom_active.svg";
+import loopSVG from "../../../public/assets/images/loop_bottom.svg";
+import avatarSVGActive from "../../../public/assets/images/avatar_bottom_active.svg";
+import avatarSVG from "../../../public/assets/images/avatar_bottom.svg";
 
 function BottomNavbar() {
   const [homeButton, setHomeButton] = useState(true);
@@ -31,26 +37,23 @@ function BottomNavbar() {
         <div className="all_image_bottom">
           <Link to="/" onClick={handleHomeButton}>
             {homeButton ? (
-              <img alt="home_nav" src="assets/images/home_bottom_active.svg" />
+              <img alt="home_nav" src={homeSVGActive} />
             ) : (
-              <img alt="home_nav" src="assets/images/home_bottom.svg" />
+              <img alt="home_nav" src={homeSVG} />
             )}
           </Link>
           <Link to="/creche/lille" onClick={handleSearchButton}>
             {searchButton ? (
-              <img alt="loop_nav" src="assets/images/loop_bottom_active.svg" />
+              <img alt="loop_nav" src={loopSVGActive} />
             ) : (
-              <img alt="loop_nav" src="assets/images/loop_bottom.svg" />
+              <img alt="loop_nav" src={loopSVG} />
             )}
           </Link>
           <Link to="/dashboard" onClick={handleDashboardButton}>
             {dashboardButton ? (
-              <img
-                alt="avatar_nav"
-                src="assets/images/avatar_bottom_active.svg"
-              />
+              <img alt="avatar_nav" src={avatarSVGActive} />
             ) : (
-              <img alt="avatar_nav" src="./assets/images/avatar_bottom.svg" />
+              <img alt="avatar_nav" src={avatarSVG} />
             )}
           </Link>
         </div>
