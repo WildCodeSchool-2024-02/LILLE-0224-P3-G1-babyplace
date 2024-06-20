4f5f827c7c13872d47fc1b0cde2353718ed80d6c
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-20 14:26:50 +0200

    fix css for homepage and header

--- client/src/components/accueil/StructureAccueil/StructureAccueil.jsx
+++ client/src/components/accueil/StructureAccueil/StructureAccueil.jsx
@@ -3,8 +3,10 @@ import "./StructureAccueil.css";
 function StructureAccueil() {
   return (
     <div className="all_structure">
+      <div className="title">
+        <h2>Vous êtes une structure d’accueil ?</h2>
+      </div>
       <div className="accueil_text">
-        <h1>Vous êtes une structure d’accueil ?</h1>
         <p>
           Equipez vous du logiciel de gestion de place d’accueil de jeunes
           enfants le plus complet
@@ -17,17 +19,16 @@ function StructureAccueil() {
           </h3>
           <p className="text_structure" id="text_structure_agenda">
             Consultez en temps reel votre agenda et modifiez le en quelques
-            cliques afin d’optimiser votre temps et votre rentabilite.
-            Accessible depuis votre smartphone ou tablette{" "}
+            cliques afin d’optimiser votre temps et votre rentabilite.{" "}
           </p>
           <img src="assets/images/1_structure.png" alt="" />
         </div>
         <div className="forimages_container">
           <h3 id="alertee_structure" className="title_structure">
-            Soyez alertee
+            Soyez alerté
           </h3>
           <p className="text_structure">
-            Choisissez le mode de notifications afin d’etre informe au plus vite
+            Choisissez le mode de notifications afin d’etre informé au plus vite
             des annulations et des demandes de reservation.
           </p>
           <img src="assets/images/2_structure.png" alt="" />
@@ -36,15 +37,15 @@ function StructureAccueil() {
           <h3 className="title_structure">Marketing de votre activité</h3>
           <p className="text_structure">
             Optimisez votre page profil pour vous rendre plus visible et vous
-            permettre de vous faire decouvrir par les parents de votre quartier.{" "}
+            permettre de vous faire decouvrir par les parents.
           </p>
           <img src="assets/images/3_structure.png" alt="" />
         </div>
         <div className="forimages_container">
-          <h3 className="title_structure">Communiquez avec les parents</h3>
+          <h3 className="title_structure">Simplifiez votre gestion</h3>
           <p className="text_structure">
-            Les parents doivent avoir preremplis leur liste de documents pour
-            pouvoir faire des réservations.
+            Tout est accessible depuis votre dashboard, avec un compte unique
+            par crèche.
           </p>
           <img src="assets/images/4_structure.png" alt="" />
         </div>
