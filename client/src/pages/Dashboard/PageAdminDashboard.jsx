c27bd8e479f862bd5b19f90f5ed14d7faf69f737
PMorganGit <morganpouilly@gmail.com>
2024-06-24 16:24:18 +0200

    Dashboard admin , only desktop

--- client/src/pages/Dashboard/PageAdminDashboard.jsx
+++ client/src/pages/Dashboard/PageAdminDashboard.jsx
@@ -3,12 +3,10 @@ import DashboardAdmin from "../../components/DashboardAdmin/DashboardAdmin";
 
 function PageDashboard() {
   return (
-    <div>
       <div>
         <Header />
         <DashboardAdmin />
       </div>
-    </div>
   );
 }
 
