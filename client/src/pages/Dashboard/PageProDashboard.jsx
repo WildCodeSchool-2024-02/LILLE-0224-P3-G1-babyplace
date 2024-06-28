14f02d71355d9adfb74921f0b70079778d0719e0
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-28 11:26:08 +0200

    fixed pagination and login page

--- client/src/pages/Dashboard/PageProDashboard.jsx
+++ client/src/pages/Dashboard/PageProDashboard.jsx
@@ -1,15 +1,17 @@
-import Header from "../../components/Header/Header";
 import DashboardPro from "../../components/DashboardPro/DashboardPro";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import BottomNavbar from "../../components/Bottom_navbar/BottomNavbar";
 
 function PageProDashboard() {
-    return (
-        <div>
-            <div>
-                <Header />
-                <DashboardPro />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <DashboardPro />
+      <Footer />
+      <BottomNavbar />
+    </div>
+  );
 }
 
 export default PageProDashboard;
--- client/src/pages/Register/nurseryRegisterPage.jsx
+++ client/src/pages/Register/nurseryRegisterPage.jsx
@@ -1,9 +1,15 @@
 import NurseryRegisterForm from "../../components/Forms/NurseryRegisterForm";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import BottomNavbar from "../../components/Bottom_navbar/BottomNavbar";
 
 function NurseriesSearchLille() {
   return (
     <div>
+      <Header />
       <NurseryRegisterForm />
+      <Footer />
+      <BottomNavbar />
     </div>
   );
 }
