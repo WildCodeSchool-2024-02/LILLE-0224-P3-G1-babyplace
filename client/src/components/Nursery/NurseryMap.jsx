da54dc7f4920a0c7c4f6985b22c9d901b7379147
VINCENT VAN VOLSEM <vincent.vanvolsem@gmail.com>
2024-07-02 10:32:37 +0200

    desktop nursery

---
+++ client/src/components/Nursery/NurseryMap.jsx
@@ -0,0 +1,31 @@
+import "./NurseryMap.css";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import PropTypes from "prop-types";
+
+function SimpleMap({ data }) {
+  const { latitude } = data;
+  const { longitude } = data;
+  const nurseryLocation = [latitude, longitude];
+
+  return (
+    <MapContainer center={nurseryLocation} zoom={13} className="map_flex">
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={nurseryLocation} />
+    </MapContainer>
+  );
+}
+
+SimpleMap.propTypes = {
+  data: PropTypes.shape({
+    nursery_name: PropTypes.string.isRequired,
+
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default SimpleMap;
