44a498e1d76c7a5b5864f858c1b98964d1eba5b7
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-07-11 14:09:09 +0200

    ajout du scroll automatique vers le haut au changement de page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import AuthContextProvider from "./context/AuthContext";
 import "./App.css";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+ 
 
 function App() {
   return (
     <AuthContextProvider>
+      <ScrollToTop /> 
       <main>
         <Outlet />
       </main>
