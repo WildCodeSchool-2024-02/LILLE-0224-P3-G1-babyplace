14e423dfc2e86d2c418ac0d42c1793eb7ff92d4e
EmilieLgt <88514461+EmilieLgt@users.noreply.github.com>
2024-07-15 22:25:03 +0200

    fix dead code

--- client/src/components/Nursery/NurseryCalendarDetails.jsx
+++ client/src/components/Nursery/NurseryCalendarDetails.jsx
@@ -116,7 +116,6 @@ export default function NurseryCalendarDetails({ bookings }) {
         {
           method: "PUT",
           headers: {
-            //  "Authorisation": user.token,
             "Content-Type": "application/json",
           },
           body: JSON.stringify(body),
