0d06f4c10307a9d2afe79a3336568eedd92dd235
ELIAS BEN BRAHIM <eliasben41@gmail.com>
2024-07-16 10:22:18 +0200

    fix css mobile footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,46 +5,54 @@ function Footer() {
   return (
     <div className="all_all_footer">
       <div className="all_footer">
-        <div className="first_part_footer">
-          <div className="icon_footer">
-            <img
-              src="../../../public/assets/images/logo.svg"
-              alt="logo_footer"
-            />
-            <p>Babyplace</p>
+        <div className="first_container_footer">
+          <div className="first_part_footer">
+            <div className="icon_footer">
+              <img
+                src="../../../public/assets/images/logo.svg"
+                alt="logo_footer"
+              />
+              <p>Babyplace</p>
+            </div>
+            <div />
           </div>
-          <div className="first_part_footer" />
-        </div>
-        <div className="second_part_footer">
-          <div className="image_footer">
-            <img src="/assets/images/logo_socials/social_fb.svg" alt="f" />
-            <img src="/assets/images/logo_socials/social_insta.svg" alt="t" />
-            <img src="/assets/images/logo_socials/social_twitter.svg" alt="i" />
+
+          <div className="second_part_footer">
+            <div className="image_footer">
+              <img src="/assets/images/logo_socials/social_fb.svg" alt="f" />
+              <img src="/assets/images/logo_socials/social_insta.svg" alt="t" />
+              <img
+                src="/assets/images/logo_socials/social_twitter.svg"
+                alt="i"
+              />
+            </div>
           </div>
         </div>
-        <div className="third_part_footer">
-          <div>
+        <div className="second_container_footer">
+          <div className="third_part_footer">
+            <div>
+              <ul>
+                <li>
+                  {" "}
+                  <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/accueil">A propos</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="fourth_part_footer">
             <ul>
               <li>
-                {" "}
-                <Link to="/contact">Contact</Link>
+                <span className="bold_text pink_text">Site réalisé par </span>
               </li>
-              <li>
-                {" "}
-                <Link to="/accueil">A propos</Link>
-              </li>
+              <li>Elias Ben Brahim | Emilie Lingat </li>
+              <li> Benoit Mezaguer | Morgan Pouilly </li>
+              <li>Vincent Van Volsem</li>
             </ul>
           </div>
-        </div>
-        <div className="fourth_part_footer">
-          <ul>
-            <li>
-              <span className="bold_text pink_text">Site réalisé par </span>
-            </li>
-            <li>Elias Ben Brahim | Emilie Lingat </li>
-            <li> Benoit Mezaguer | Morgan Pouilly </li>
-            <li>Vincent Van Volsem</li>
-          </ul>
         </div>
       </div>
       <div className="credits_footer">
